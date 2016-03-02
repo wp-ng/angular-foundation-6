c3dd7e77ee55f73bab54e315b8ebed82b280f8de
Rickert Mulder <circlingthesun@gmail.com>
2016-03-02 15:45:47 +0200

    refactor(alert): Ported to F6

--- src/alert/docs/demo.js
+++ src/alert/docs/demo.js
@@ -1,11 +1,11 @@
 angular.module('foundationDemoApp').controller('AlertDemoCtrl', function($scope) {
   $scope.alerts = [
-    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+    { type: 'alert', msg: 'Oh snap! Change a few things up and try submitting again.' },
     { type: 'success round', msg: 'Well done! You successfully read this important alert message.' }
   ];
 
   $scope.addAlert = function() {
-    $scope.alerts.push({msg: "Another alert!"});
+    $scope.alerts.push({type: 'alert', msg: "Another alert!"});
   };
 
   $scope.closeAlert = function(index) {

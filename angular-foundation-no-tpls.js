772991b4d6876d9870957ba772de25f16409bb84
Rickert Mulder <circlingthesun@gmail.com>
2016-07-06 23:19:46 +0200

    fix(docs): Remove warning

--- angular-foundation-no-tpls.js
+++ angular-foundation-no-tpls.js
@@ -9,7 +9,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
  * angular-foundation-6
  * http://circlingthesun.github.io/angular-foundation-6/
 
- * Version: 0.9.40 - 2016-07-06
+ * Version: 0.9.41 - 2016-07-06
  * License: MIT
  * (c) 
  */
